c849d1c065174851571fd5ac4dcedcd4d7d2b153
Tsuki <audeeccher@gmail.com>
2017-01-10 15:11:59 +0100

    moins crade

--- js/a.js
+++ js/a.js
@@ -1,15 +1,27 @@
-const seuil=200;
+
+var stop;
+var seuil
+seuil = prompt("donner le nombre de case souhaité")
+seuil = parseInt(seuil, 10)
+/*alert(stop)*/
+
+for (var i = 1; i <= seuil; i++)
+	{
+		document.getElementById('liste').innerHTML += "<li>"+i+"</li>";	
+}
+
+/*const seuil=200;
 var stop;
 
 stop = prompt("donner le nombre de case souhaité")
 stop = parseInt(stop, 10)
 /*alert(stop)*/
 
-for (var i = 1; i <= seuil; i++)
+/*for (var i = 1; i <= seuil; i++)
 	{
 		document.getElementById('liste').innerHTML += "<li>"+i+"</li>";
 		if (i==stop){i+=200
 		}
+		
 	
-}
-
+}*/
