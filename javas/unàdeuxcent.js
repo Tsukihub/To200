f459ab51ee8e021c302d8160a11b82b3b5d126c8
Tsuki <audeeccher@gmail.com>
2017-01-10 12:00:17 +0100

    algo début js

---
+++ javas/unàdeuxcent.js
@@ -0,0 +1,23 @@
+const seuil=200;
+Var numCase= parseInt(numCase)
+
+function AjoutCase()
+{for (var numCase = 1; numCase < seuil; seuil++) {
+	document.getElementById("addhere").innerHTML
+}
+
+}
+
+
+	Pour numCase Allant de 1 A SEUIL Au pas de 1
+	 	getElementsById.ul.innerhtml("<li>",numCase,"</li>")
+	 	
+	 	Fpour 
+Fin
+
+
+
+  for (i = 0; i != a.length; i++)
+    resultat[i] = f(a[i]);
+  return resultat;
+}
